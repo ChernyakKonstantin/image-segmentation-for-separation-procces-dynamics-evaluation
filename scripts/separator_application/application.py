590052324be489468f1a4f0f59ffb917d53eac13
ChernyakKonstantin <hernyakonstantin@gmail.com>
2021-02-13 22:40:31 +0700

    Добавить функция записи в поле text

--- scripts/separator_application/application.py
+++ scripts/separator_application/application.py
@@ -2,8 +2,7 @@ import csv
 import tkinter as tk
 from queue import Queue
 from threading import Thread
-from time import time
-from tkinter import ttk
+from time import time, sleep
 
 import cv2 as cv
 from PIL import Image, ImageTk
@@ -25,6 +24,38 @@ def write_csv(file_path, data):
         writer.writerow(data)
 
 
+class CameraThread(Thread):
+    """Поток для считывания изображения с камеры."""
+
+    def __init__(self, queue, capture, period, image_size, *args, **kwargs):
+        """
+
+        Args:
+            queue (queue.Queue):
+            capture (cv2.VideoCapture):
+            period (float): Период считвания с камеры (в секундах)
+        """
+        super().__init__(*args, **kwargs)
+        self.queue = queue
+        self.capture = capture
+        self.period = period
+        self.image_width, self.image_height = image_size
+
+    def run(self):
+        t_1 = time()
+        while True:
+            if time() - t_1 >= self.period:
+                _, image = self.capture.read()
+                image = cv.resize(image, (self.image_width, self.image_height))
+                image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+                image = Image.fromarray(image)
+                image = ImageTk.PhotoImage(image)
+
+                self.queue.put(image)
+                t_1 = time()
+
+
 class Application(tk.Tk):
     """Класс оконного приложения
 
@@ -42,21 +73,31 @@ class Application(tk.Tk):
         self.capture = cv.VideoCapture(constants.DEVICE_ID, cv.CAP_ANY)
         self.capture.set(cv.CAP_PROP_FRAME_WIDTH, constants.CAMERA_RESOLUTION[0])
         self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, constants.CAMERA_RESOLUTION[1])
+        self.write_to_status_panel('Capture is ready')
+
+    def model_setup(self):
+        sleep(15)
+        self.write_to_status_panel('Model is ready')
 
     def run_camera(self):
         self.camera_thread = CameraThread(
             self.queue,
             self.capture,
             constants.PERIOD,
-            self.mf.image.get_canvas_size()
+            self.mainframe.image.get_canvas_size()
         )
         self.camera_thread.start()
+        self.write_to_status_panel('Camera thread is ready')
 
     def check_queue(self):
         if not self.queue.empty():
             self.image = self.queue.get()
-            self.mf.image.create_image(0, 0, anchor='nw', image=self.image)
+            self.mainframe.image.create_image(0, 0, anchor='nw', image=self.image)
+        self.write_to_status_panel('Updated')
         self.after(1, self.check_queue)
+
+    def write_to_status_panel(self, text):
+        self.mainframe.info.write_new_line(text)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -66,52 +107,26 @@ class Application(tk.Tk):
 
         self.title('Separation Dynamics')
 
-        self.mf = w.MainFrame(self)
-        self.mf.grid(sticky=tk.NSEW)
+        self.mainframe = w.MainFrame(self)
+        self.mainframe.grid(sticky=tk.NSEW)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
+        self.update_idletasks()
+
         self.queue = Queue()
+
         self.camera_setup()
 
         self.run_camera()
 
-        self.after(100, self.check_queue)
+        # self.model_setup()
 
+        self.check_queue()
 
-class CameraThread(Thread):
-    """Поток для считывания изображения с камеры."""
-
-    def __init__(self, queue, capture, period, image_size, *args, **kwargs):
-        """
-
-        Args:
-            queue (queue.Queue):
-            capture (cv2.VideoCapture):
-            period (float): Период считвания с камеры (в секундах)
-        """
-        super().__init__(*args, **kwargs)
-        self.queue = queue
-        self.capture = capture
-        self.period = period
-        self.image_width, self.image_height = image_size
-        print(self.image_width, self.image_height)
-
-    def run(self):
-        t_1 = time()
-        while True:
-            if time() - t_1 >= self.period:
-                _, image = self.capture.read()
-                image = cv.resize(image, (self.image_width, self.image_height))
-                image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-
-                image = Image.fromarray(image)
-                image = ImageTk.PhotoImage(image)
-
-                self.queue.put(image)
-                t_1 = time()
 
 
 if __name__ == '__main__':
     app = Application()
     app.mainloop()
+
