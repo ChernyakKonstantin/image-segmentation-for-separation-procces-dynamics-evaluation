50127a590219a956dcba74ff333d7a4f435f461f
ChernyakKonstantin <hernyakonstantin@gmail.com>
2021-04-19 01:03:34 +0700

    Использовать встроенный класс TCPServer и создать класс обработчика запросов.

--- scripts/server.py
+++ scripts/server.py
@@ -1,56 +1,23 @@
-"""Пример из справочника (а вообще он вроде бы из документации)"""
+"""Модуль сервера, запускаемый на ПК, выполняющем сегментацию изображений."""
 
-import socket
 import struct
+from socketserver import BaseRequestHandler, TCPServer
+from typing import Tuple
 
 import numpy as np
-from typing import Tuple
 
 from image_processor import ImageProcessor
 
 
-class JetsonServer:
-    """Класс сервера, запускаемого на Nvidia Jetson Nano.
-    Функции:
-    1) Опрос камеры,
-    2) Сегментация изображения,
-    3) Вычисление соотношения площадей сегментированных областей,
-    4) Отправка результатов клиенту.
+class JetsonServerHandler(BaseRequestHandler):
+    """Класс обработчика запросов.
+    Возвращает результат сегментации: кортеж из изображения маски фракций и кортежа долей их площадей.
     """
-    _server_address: Tuple[str, int]
-    _image_processor: ImageProcessor
-
-    def __init__(self,
-                 ip: str,
-                 port: int,
-                 ):
+    def __init__(self, *args, **kwargs):
+        super(JetsonServerHandler, self).__init__(*args, **kwargs)
         self._image_processor = ImageProcessor()
-        self._server_address = (ip, port)
-        self._setup()
-
-    def _setup(self):
-        """Метод предназначен для:
-        1) Создания сокета,
-        2) Привзяки сокета к прослушиваемому порту
-        """
-        self._sock = socket.socket(socket.AF_INET. socket.SOCK_STREAM)
-        self._sock.bind(self._server_address)
-        self._sock.listen(1)
-        print(f'Server started on {self._server_address[0]} port: {self._server_address[1]}')
-
-    def run(self):
-        self._wait_connection()
-        self._send()
-        self._close()
-
-    def _wait_connection(self):
-        """Метод ожидает подключения от клиента."""
-        while True:
-            print('waiting for а connection')
-            self._connection, client_address = self._sock.accept()
-            print(f'Got connection from {client_address}')
-
-    def _send(self):
+    
+    def handle(self):
         mask: np.ndarray
         ratio: Tuple[float, float, float]
         byte_mask: bytes
@@ -61,8 +28,11 @@ class JetsonServer:
         byte_mask = mask.tobytes()
         byte_ratio = struct.pack('3f', *ratio)
 
-        self._connection.sendall(byte_mask)
+        self.request.sendall(byte_ratio)
 
-    def _close(self):
-        """Метод закрывает соединение"""
-        self._connection.close()
+
+if __name__ == '__main__':
+    HOST, PORT = 'localhost', 80
+
+    with TCPServer((HOST, PORT), JetsonServerHandler) as server:
+        server.serve_forever()
