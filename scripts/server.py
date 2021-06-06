bf747f274689a8d11cf19723b4b701ced8c100d0
ChernyakKonstantin <hernyakonstantin@gmail.com>
2021-06-06 11:55:43 +0700

    Исправить класс сервера

--- scripts/server.py
+++ scripts/server.py
@@ -1,6 +1,6 @@
 """Модуль сервера, запускаемый на ПК, выполняющем сегментацию изображений."""
 
-import struct
+import pickle
 from os import path
 from socketserver import BaseRequestHandler, TCPServer
 from typing import Tuple
@@ -82,29 +82,24 @@ class ImageProcessor:
         pred = self._predict(image)
         mask = ImageProcessor._create_mask(pred)
         oil_fraction, emulsion_fraction, water_fraction = ImageProcessor._calc_fractions(mask)
-        return mask, (oil_fraction, emulsion_fraction, water_fraction)
+        return image, mask, (oil_fraction, emulsion_fraction, water_fraction)
 
 
 class JetsonServerHandler(BaseRequestHandler):
     """Класс обработчика запросов.
     Возвращает результат сегментации: кортеж из изображения маски фракций и кортежа долей их площадей.
     """
-    def __init__(self, *args, **kwargs):
-        super(JetsonServerHandler, self).__init__(*args, **kwargs)
-        self._image_processor = ImageProcessor()
+    _image_processor = ImageProcessor()
 
     def handle(self):
         mask: np.ndarray
-        ratio: Tuple[float, float, float]
-        byte_mask: bytes
-        byte_ratio: bytes
+        values: Tuple[float, float, float]
 
-        mask, ratio = self._image_processor.process_frame()
-        # TODO найти способ объединить эти элементы для совместной отправки
-        byte_mask = mask.tobytes()
-        byte_ratio = struct.pack('3f', *ratio)
+        image, mask, values = self._image_processor.process_frame()
 
-        self.request.sendall(byte_ratio)
+        package = pickle.dumps((image, mask, values))
+        print((type(package), len(package)))
+        self.request.sendall(package)
 
 
 if __name__ == '__main__':
