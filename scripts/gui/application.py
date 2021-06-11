c2580d8e6ea45b86a6b8b3ecb8f0be0551a4b992
ChernyakKonstantin <hernyakonstantin@gmail.com>
2021-06-11 08:46:12 +0700

    Добавить логгирование результатов эксперимента и сохранение их визуализации

--- scripts/gui/application.py
+++ scripts/gui/application.py
@@ -6,16 +6,18 @@ import pickle
 import socket
 import sys
 from typing import Any
-
+import os
 from PyQt5.QtCore import QTimer
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QActionGroup, QMenu, QPushButton
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QActionGroup, QMenu, QPushButton, QLabel
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtWidgets import QHBoxLayout, QMenuBar, QStatusBar, QVBoxLayout
-
-from interactive_chart import BaseTimeSeries
+from PyQt5.QtWidgets import QApplication, QCalendarWidget, QCheckBox, QFileDialog, QLabel, QMainWindow, QTimeEdit
+import matplotlib.pyplot as plt
+from interactive_chart import TimeSeriesChart
 from interactive_mask_display import InteractiveMaskDisplay
-
+import csv
 
 class Client:
     """Класс TCP-клиента, опрашивающего сервер для получения результатов сегментации."""
@@ -35,123 +37,146 @@ class Client:
                     break
                 request.append(packet)
             package = pickle.loads(b"".join(request))
-            image, mask, values = package
+            # image, mask, values = package
+            image, values = package
 
         cur_datetime = dt.datetime.now()
-        return image, mask, cur_datetime, values
+        # return image, mask, cur_datetime, values
+        return image, cur_datetime, values
 
 
 class CentralWidget(QWidget):
     """Обязательный виджет Qt-приложения. Используется в QMainWindow."""
 
+    empty_message = 'Unknown'
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._setup_menu_pbtn()
+        self.chart = TimeSeriesChart()
+        self.img = InteractiveMaskDisplay()
+        self.segmented_img = InteractiveMaskDisplay()
 
-        self.chart = BaseTimeSeries()
-        self.segmented_img = InteractiveMaskDisplay('Segmentation')
+        self.oil_label = QLabel(f'Oil fraction: {self.empty_message}')
+        self.emulsion_label = QLabel(f'Emulsion fraction: {self.empty_message}')
+        self.water_label = QLabel(f'Water fraction: {self.empty_message}')
+
+        label_layout = QVBoxLayout()
+        label_layout.addWidget(self.oil_label)
+        label_layout.addWidget(self.emulsion_label)
+        label_layout.addWidget(self.water_label)
 
         h_layout = QHBoxLayout()
+        h_layout.addWidget(self.img)
         h_layout.addWidget(self.segmented_img)
+        h_layout.addLayout(label_layout)
+        h_layout.setAlignment(Qt.AlignLeft)
 
         v_layout = QVBoxLayout()
         v_layout.addWidget(self.chart)
         v_layout.addLayout(h_layout)
-        v_layout.addWidget(self.menu_pbtn, 1, Qt.AlignLeft)
 
         self.setLayout(v_layout)
 
-    def _setup_visibility_menu(self):
-        self.menu = QMenu('Select visible layers')
-        self._action_group = QActionGroup(self)
-        self._action_group.setExclusive(False)
-
-        self._visibility_menu_actions = {}
-        for action_name in ['All', 'Oil', 'Emulsion', 'Water']:
-            action = self.menu.addAction(action_name)
-            action.setCheckable(True)
-            if action_name == 'All':
-                action.triggered.connect(self._on_show_all_trigger)
-            else:
-                action.triggered.connect(self._on_visibility_menu_trigger)
-                self._action_group.addAction(action)
-            self._visibility_menu_actions[action_name] = action
-
-    def _setup_menu_pbtn(self):
-        self._setup_visibility_menu()
-        self.menu_pbtn = QPushButton('Visible layers')
-        self.menu_pbtn.setMenu(self.menu)
-
-    def _on_show_all_trigger(self):
-        """Обработчик нажатия на кнопку All"""
-        if self._visibility_menu_actions['All'].isChecked():
-            for action in self._action_group.actions():
-                action.setChecked(True)
-        else:
-            for action in self._action_group.actions():
-                action.setChecked(False)
-
-    def _on_visibility_menu_trigger(self):
-        """Обработчик нажатия на кнопки"""
-        if all([action.isChecked() for action in self._action_group.actions()]):
-            self._visibility_menu_actions['All'].setChecked(True)
-        else:
-            self._visibility_menu_actions['All'].setChecked(False)
-
-        param_to_be_passed = True
-        self.layer_change_handler(param_to_be_passed)
-
-    def layer_change_handler(self, param_to_be_passed: Any):
-        """Обработчик, вызываемые при нажатии внутри меню выбора слоев.
-        Предназачен для вызова методов экзеплеров класса графика временного ряда
-        и класса отображения изображения.
-        """
-        self.chart.demo_func(param_to_be_passed)
-        self.segmented_img.demo_func(param_to_be_passed)
+    def set_text(self, values: tuple):
+        self.oil_label.setText(f'Oil fraction: {round(values[0], 1)}%')
+        self.emulsion_label.setText(f'Emulsion fraction: {round(values[1], 1)}%')
+        self.water_label.setText(f'Water fraction: {round(values[2], 1)}%')
 
 
 class MainWindow(QMainWindow):
     """Обязательный виджет Qt-приложения. Используется в QApplication."""
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.setWindowTitle('Separator Dynamics Estimator')
 
         self.status_bar = QStatusBar()
         self.setStatusBar(self.status_bar)
+        self.status_bar.showMessage('Server is disconnected')
 
         self.menu_bar = QMenuBar()
         self.setMenuBar(self.menu_bar)
         self.menu_bar.addAction('Quit', self.close)
+        self.menu_bar.addSeparator()
 
         self.central_widget = CentralWidget()
         self.setCentralWidget(self.central_widget)
 
-        self.show()
+        self.showFullScreen()
+
+    def add_menubar_action(self, action_name, callback):
+        self.menu_bar.addAction(action_name, callback)
+        self.menu_bar.addSeparator()
 
 
 class Application(QApplication):
+    LOG_DIRECTORY = 'logs'
+    FIELD_NAMES = ('oil', 'emulsion', 'water')
+
+    file_path = None
+    call_period = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._tcp_client_setup()
+        self._setup_logger()
         self.main_window = MainWindow()
+        self.main_window.add_menubar_action('Connect to server', self._tcp_client_setup)
+        self.main_window.add_menubar_action('Save chart', self._handle_image_save)
+
+    def _setup_logger(self):
+        """Инициализация файла лога текущего эксперимента"""
+        if not os.path.exists(self.LOG_DIRECTORY):
+            os.mkdir(self.LOG_DIRECTORY)
+        current_date = dt.datetime.now().strftime('%d-%m-%y-%H-%M')
+        filename = f'{current_date}_experiment_log.csv'
+        self.file_path = os.path.join(self.LOG_DIRECTORY, filename)
+        log_file = open(self.file_path, 'w')
+        log_file.close()
+
+    def _handle_log_write(self, values: dict):
+        with open(self.file_path, 'a+', newline='') as log_file:
+            writer = csv.DictWriter(log_file, fieldnames=self.FIELD_NAMES)
+            writer.writerow(values)
 
     def _tcp_client_setup(self):
         """Инициализация TCP-клиента"""
         self._tcp_client = Client('localhost', 80)
         self._tcp_client_timer = QTimer()
-        self._tcp_client_timer.setInterval(100)
-        self._tcp_client_timer.timeout.connect(self._handle_tcp_client)
+        self._tcp_client_timer.setInterval(self.call_period)
+        self._tcp_client_timer.timeout.connect(self._handle_tcp_client_request)
         self._tcp_client_timer.start()
 
-    def _handle_tcp_client(self):
+    def _handle_tcp_client_request(self):
         """Обработчик TCP-клиента"""
-        image, mask, cur_time, values = self._tcp_client.receive()
-        print(mask.shape)
-        self.main_window.central_widget.chart.add_new_values((cur_time, values))
-        self.main_window.central_widget.segmented_img.draw(image)
+        try:
+            image, cur_time, values = self._tcp_client.receive()
+            self.main_window.central_widget.chart.add_new_values((cur_time, values))
+            self.main_window.central_widget.set_text(values)
+            self.main_window.central_widget.segmented_img.draw(image)
+            self.main_window.status_bar.showMessage('Server is connected')
+        except ConnectionRefusedError:
+            self._tcp_client_timer.stop()
+            self.main_window.status_bar.showMessage('Server is disconnected')
+
+    def _handle_image_save(self):
+        filename = QFileDialog.getSaveFileName(None, 'Open File', './', "Image (*.png *.jpg *jpeg)")[0]
+
+        with open(self.file_path, 'r') as log_file:
+            reader = csv.DictReader(log_file, fieldnames=self.FIELD_NAMES)
+            data_to_plot = {name: [] for name in self.FIELD_NAMES}
+            for row in reader:
+                for key, value in row.items():
+                    data_to_plot[key].append(value)
+
+        fig, ax = plt.subplots(1)
+        for key, value in data_to_plot.items():
+            ax.plot(value, label=key)
+        ax.set_xlabel('Time, minutes')
+        ax.set_ylabel('Fraction, %')
+        ax.grid(True)
+        ax.legend()
+
+        fig.savefig(filename)
 
 
 app = Application(sys.argv)
